7f85184df959a7ae1665d0c15a6e2f8dee60c8fe
enigma <linuzzz7@yandex.ru>
2024-03-31 13:30:19 +0300

    code update

--- PythonEtHack2024/network_scanner.py
+++ PythonEtHack2024/network_scanner.py
@@ -2,6 +2,10 @@
 import scapy.all as scapy
 
 def scan(ip):
-    scapy.arping(ip)
+    arp_request = scapy.ARP()  # создаем объект запроса
+    arp_request.pdst = ip  # подставляется IP-адрес из параметров
+    print(arp_request.summary())  # общая информация о ARP-запросе
+    scapy.ls(scapy.ARP())
 
-scan('192.168.1.0/24')
+
+scan('192.168.1.254')
