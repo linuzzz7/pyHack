a698ea96f58127a0a114cf35334839641b1d2156
enigma <linuzzz7@yandex.ru>
2024-04-02 12:00:42 +0300

    network_scan final 1.0

--- PythonEtHack2024/network_scanner.py
+++ PythonEtHack2024/network_scanner.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 import scapy.all as scapy
+import argparse
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--target', dest='target', help='Target IP / IP range.')
+    options = parser.parse_args()
+    return options
 
 
 def scan(ip):
@@ -8,9 +16,19 @@ def scan(ip):
     arp_request_broadcast = broadcast/arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
-    print('IP\t\t\tMAC Address\n'+'-'*50)
+    clients_list = []  # создаем список, и будем добавлять в него словари
     for element in answered_list:
-        print(element[1].psrc + '\t\t' + element[1].hwsrc)
+        client_dict = {'ip': element[1].psrc, 'mac': element[1].hwsrc}
+        clients_list.append(client_dict)
+    return clients_list
 
 
-scan('10.240.1.0/24')
+def print_result(results_list):
+    print('IP\t\t\tMAC Address\n'+'-'*50)
+    for client in results_list:
+        print(client['ip'] + '\t\t' + client['mac'])
+
+
+options = get_arguments()
+scan_result = scan(options.target)
+print_result(scan_result)
