28c08cbf2771275af8ea1888540fce92b6bddb1b
enigma <linuzzz7@yandex.ru>
2024-04-02 11:53:43 +0300

    network scan final 0.1

--- PythonEtHack2024/network_scanner.py
+++ PythonEtHack2024/network_scanner.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 import scapy.all as scapy
 
+
 def scan(ip):
-    arp_request = scapy.ARP()  # создаем объект запроса
-    arp_request.pdst = ip  # подставляется IP-адрес из параметров
-    print(arp_request.summary())  # общая информация о ARP-запросе
-    scapy.ls(scapy.ARP())
+    arp_request = scapy.ARP(pdst=ip)  # создаем объект запроса
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    print('IP\t\t\tMAC Address\n'+'-'*50)
+    for element in answered_list:
+        print(element[1].psrc + '\t\t' + element[1].hwsrc)
 
 
-scan('192.168.1.254')
+scan('10.240.1.0/24')
