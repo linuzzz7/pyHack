1ab7722332d580bb762e69a42cd8e568db540678
enigma <linuzzz7@yandex.ru>
2024-04-09 09:47:11 +0300

    Add You tube downloader

--- PythonEtHack2024/file_intersepter.py
+++ PythonEtHack2024/file_intersepter.py
@@ -1,2 +1,20 @@
 #!/usr/bin/env python
+import netfilterqueue
 import scapy.all as scapy
+
+
+def process_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload())
+    if scapy_packet.haslayer(scapy.Raw):
+        if scapy_packet[scapy.TCP].dport == 80: # будет запросом
+            print('HTTP Request')
+            print(scapy_packet.show())
+        elif scapy_packet[scapy.tcpdump].sport == 80: # будет ответом
+            print('HTTP Response')
+            print(scapy_packet.show())
+    packet.accept()
+
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet())
+queue.run()
---
+++ interest/youtube_download.py
@@ -0,0 +1,28 @@
+from pytube import YouTube
+from pydub import AudioSegment
+
+# Введите ссылку на YouTube видео
+url = input("Введите ссылку на YouTube видео: ")
+
+# Создание объекта YouTube
+yt = YouTube(url)
+
+type_file = int(input('Выбор:\n1. Если надо загрузить Аудио - mp3.\n2. Если надо загрузить Видео - mp4.\n>>> '))
+
+if type_file == 1:
+    print("Загрузка аудио...")
+    audio = yt.streams.filter(only_audio=True).first()
+    audio_file = audio.download()
+    print("Аудио загружено!")
+    # Конвертация в MP3
+    print("Конвертация в MP3...")
+    song = AudioSegment.from_file(audio_file, format="mp4")
+    song.export(f"{yt.title}.mp3", format="mp3")
+    print("Конвертация завершена!")
+elif type_file == 2:
+    # Загрузите видео
+    print("Загрузка видео...")
+    yt.streams.first().download()
+    print("Видео загружено!")
+else:
+    print('Неверный выбор!')
