b61f3659fe2e710e07b5048c02e7b8ad54bf9330
enigma <linuzzz7@ya.ru>
2024-06-28 09:13:18 +0300

    add test file

--- tests/test1.py
+++ tests/test1.py
@@ -1,1 +1,7 @@
-
+import subprocess
+a = '17.1.1.1'
+result = subprocess.run(['ping', a, '-c', '3'], stderr=subprocess.PIPE, encoding='utf-8')
+# Теперь в result.stdout пустая строка, а в result.stderr находится стандартный поток вывода:
+print(result.stdout)
+print(result.stderr)
+print(result.returncode)
